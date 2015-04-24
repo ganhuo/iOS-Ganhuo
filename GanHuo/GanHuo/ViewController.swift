fb34ef16c6a47084d5dc1ee14a3996fbf209ec47
callmewhy <whywanghai@gmail.com>
2015-04-24 16:05:18 +0800

    init xcode

---
+++ GanHuo/GanHuo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GanHuo
+//
+//  Created by why on 4/24/15.
+//  Copyright (c) 2015 why. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
